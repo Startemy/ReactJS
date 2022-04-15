de0320a7470688d27a0ab787106a38cda5b146d1
Artem Alaev <alayev7@gmail.com>
2022-04-15 18:39:22 +0300

    Убрал фрагмент textarea в Input

--- src/components/Func/Input.jsx
+++ src/components/Func/Input.jsx
@@ -1,7 +1,6 @@
 export const Input = (props) => {
-  return <>
-    <textarea autoFocus type="text" onKeyDown={props.onKey}
-      value={props.value}
-      onChange={props.change} placeholder="Тут пусто" />
-  </>
+  return <textarea autoFocus type="text" onKeyDown={props.onKey}
+    value={props.value}
+    onChange={props.change} placeholder="Тут пусто">
+  </ textarea>
 }
