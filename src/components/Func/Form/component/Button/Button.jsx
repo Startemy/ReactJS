6eb44fd86078b64dde7f7c60f476745a8be1fd2e
Artem Alaev <alayev7@gmail.com>
2022-04-23 20:48:04 +0300

    Добавил loki, coverage и тест

--- src/components/Func/Form/component/Button/Button.jsx
+++ src/components/Func/Form/component/Button/Button.jsx
@@ -4,7 +4,7 @@ import PaperAir from 'images/buttons/Paperair.png';
 export const Button = ({ onButtonClick }) => {
   return (
     <button type="submit" data-testid="btn-send" onClick={onButtonClick}>
-      <img src={PaperAir}></img>
+      <img src={PaperAir} alt="Отправить сообщение"></img>
     </button>
   );
 };
